690ae1fa5e76dbe5e2dc49542bfddf62eddfad31
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-12-07 00:51:29 -0500

    Added fade animation and fixed cupid shuffle fragment texts.

--- app/src/main/java/org/ohmstheresistance/aboutomar/fragments/CupidShuffleFragment.java
+++ app/src/main/java/org/ohmstheresistance/aboutomar/fragments/CupidShuffleFragment.java
@@ -1,11 +1,16 @@
 package org.ohmstheresistance.aboutomar.fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.ohmstheresistance.aboutomar.R;
 
@@ -13,6 +18,7 @@ public class CupidShuffleFragment extends Fragment {
 
 
     private View rootView;
+    private TextView cupidShuffleLinkTextView;
 
     public CupidShuffleFragment() {
         // Required empty public constructor
@@ -24,8 +30,25 @@ public class CupidShuffleFragment extends Fragment {
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_cupid_shuffle, container, false);
 
+        cupidShuffleLinkTextView = rootView.findViewById(R.id.cupid_shuffle_link_textview);
+
 
         return rootView;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        cupidShuffleLinkTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String linkedInURL = "https://github.com/InquisitiveMindHasToKnow/CupidShuffle";
+
+                Intent viewLinkIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedInURL));
+                startActivity(viewLinkIntent);
+            }
+        });
+    }
 }
