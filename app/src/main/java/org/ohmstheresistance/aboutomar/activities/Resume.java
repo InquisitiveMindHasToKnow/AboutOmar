36faab7cd91db607214ad5b43de2f41916be4296
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-12-07 14:39:39 -0500

    Added correct images for cupid shuffle fragment. Removed action bar

--- app/src/main/java/org/ohmstheresistance/aboutomar/activities/Resume.java
+++ app/src/main/java/org/ohmstheresistance/aboutomar/activities/Resume.java
@@ -2,16 +2,22 @@ package org.ohmstheresistance.aboutomar.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import org.ohmstheresistance.aboutomar.R;
 
 public class Resume extends AppCompatActivity {
 
+    private TextView pursuitExperienceTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
+
+        pursuitExperienceTextview = findViewById(R.id.pursuit_experience_textview);
+      //  pursuitExperienceTextview.startAnimation(AnimationUtils.loadAnimation(Resume.this, R.anim.slide_in_left));
 
 
     }
